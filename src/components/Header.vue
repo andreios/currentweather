d99c048bf0d8e328cf6398756dd63205a8c11339
andreios <45299090+andreios@users.noreply.github.com>
2020-12-14 08:01:18 -0600

    initial push

---
+++ src/components/Header.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="header">
+    <h1>{{title}}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Header",
+  props: ['title']
+};
+</script>
+
+<style scoped>
+.header {
+  font-family: neue-kabel, sans-serif;
+  font-weight: 700;
+  font-style: normal;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  background: rgb(28, 69, 102);
+  color: white;
+ }
+</style>
